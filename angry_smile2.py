20892ce42ea2673825d9a92c026bb5be6154facd
Pleshakova <ufimtseva.olga@gmail.com>
2020-10-05 15:08:39 +0300

    Рисую злобного смайла

---
+++ angry_smile2.py
@@ -0,0 +1,15 @@
+def main():
+    screen=(400,400)
+    x=200
+    y=200
+    r=100
+    draw_angry_smile(x,y,r)
+def draw_angry_smile(x,y,r):
+    """ Рисую злобного смайла
+    x- координата x центра окружности
+    y- координала y центра окружности
+    r- радиус окружности
+    """
+    pass
+    print("Типа рисую angry_smile", x,y,r)
+main()
